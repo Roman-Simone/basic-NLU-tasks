d733bcbca7d16b0acecc3deb4018aa7d9808dcbf
Roman Simone <83060406+Sro552@users.noreply.github.com>
2024-05-09 09:29:18 +0200

    last update

--- exam/247181_simone_roman/NLU/part_1/main.py
+++ exam/247181_simone_roman/NLU/part_1/main.py
@@ -3,6 +3,9 @@
 
 # Import everything from functions.py file
 from functions import *
+from utils import *
+from model import *
+import os
 
 if __name__ == "__main__":
     #Wrtite the code to load the datasets and to run your functions
