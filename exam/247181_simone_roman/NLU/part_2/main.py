fe3733fbb126d4ab2025e9a9174e3437959bf22d
Roman Simone <83060406+Sro552@users.noreply.github.com>
2024-05-14 17:59:42 +0200

    only 1 run

--- exam/247181_simone_roman/NLU/part_2/main.py
+++ exam/247181_simone_roman/NLU/part_2/main.py
@@ -59,39 +59,36 @@ if __name__ == "__main__":
     criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
     criterion_intents = nn.CrossEntropyLoss() # Because we do not have the pad token
 
-    n_epochs = 10
-    runs = 1
+        
+    n_epochs = 20
+    patience = 3
+    losses_train = []
+    losses_dev = []
+    sampled_epochs = []
+    best_f1 = 0
+    for x in tqdm(range(1,n_epochs)):
+        loss = train_loop(train_loader, optimizer, criterion_slots, 
+                        criterion_intents, model, clip=clip)
+        if x % 5 == 0: # We check the performance every 5 epochs
+            sampled_epochs.append(x)
+            losses_train.append(np.asarray(loss).mean())
+            results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
+                                                        criterion_intents, model, lang)
+            losses_dev.append(np.asarray(loss_dev).mean())
+            
+            f1 = results_dev['total']['f']
+            # For decreasing the patience you can also use the average between slot f1 and intent accuracy
+            if f1 > best_f1:
+                best_f1 = f1
+                # Here you should save the model
+                patience = 3
+            else:
+                patience -= 1
+            if patience <= 0: # Early stopping with patience
+                break # Not nice but it keeps the code clean
 
-    slot_f1s, intent_acc = [], []
-
-    for x in tqdm(range(0, runs)):
-        model = ModelBert(hid_size, out_slot, out_int).to(device)
-        optimizer = optim.Adam(model.parameters(), lr=lr)
-        criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
-        criterion_intents = nn.CrossEntropyLoss() # Because we do not have the pad token
-
-        patience = 3
-        losses_train = []
-        losses_dev = []
-        sampled_ephocs = []
-        best_f1 = 0
-
-        for epoch in range(1, n_epochs):
-            loss = train_loop(train_loader, optimizer, criterion_slots, 
-                            criterion_intents, model)
-
-            if x % 5 == 0:
-                sampled_ephocs.append(x)
-                losses_train.append(np.asarray(loss).mean())
-                eval_loop(dev_loader, criterion_slots, criterion_intents, model, lang)
-                results_dev, intent_res, loss_dev = losses_dev.append(np.asarray(loss_dev).mean())
-                f1 = results_dev['total']['f']
-
-                if f1 > best_f1:
-                    best_f1 = f1
-                else:
-                    patience -= 1
-                if patience <= 0: # Early stopping with patient
-                    break # Not nice but it keeps the code clean
-
+    results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
+                                            criterion_intents, model, lang)    
+    print('Slot F1: ', results_test['total']['f'])
+    print('Intent Accuracy:', intent_test['accuracy'])
 
