c9d1cd14cae83af8c1c9aa6ad483b255c5dc0184
Roman Simone <83060406+Sro552@users.noreply.github.com>
2024-06-05 18:22:37 +0200

    update

---
+++ exam/247181_Simone_Roman/LM/part_2/extra/test_model.py
@@ -0,0 +1,58 @@
+import sys
+import pathlib
+from functools import partial
+from torch.utils.data import DataLoader
+
+# directory reach
+directory = pathlib.Path(__file__).parent.resolve()
+sys.path.append(str(directory.parent))
+
+from utils import *
+from model import *
+from functions import *  # Import everything from functions.py file
+
+
+# Main function FOR TESTING
+if __name__ == "__main__":
+
+    # NAME OF THE MODEL to load
+    # name_model = "model_21.pt" -> weight_tying 
+    # name_model = "model_22.pt" -> Variational_Dropout 
+    # name_model = "model_23_auto.pt" -> AvSGD_auto 
+    # name_model = "model_23_manual.pt" -> AvSGD_manual
+    name_model = "model_23_manual.pt"
+    path_model_saved = f"{directory.parent}/bin/{name_model}"
+
+
+    # IPERPARAMETERS 
+    config = {
+        "batch_size_test": 128,
+        "hid_size": 500,
+        "emb_size": 500,
+    }
+    
+    # LOADING DATA
+    path_dataset = directory.parent
+    train_raw = read_file(os.path.join(path_dataset, "dataset/PennTreeBank/ptb.train.txt"))
+    test_raw = read_file(os.path.join(path_dataset, "dataset/PennTreeBank/ptb.test.txt"))
+
+    #get vocab and trasform in id
+    vocab = get_vocab(train_raw, ["<pad>", "<eos>"])
+    lang = Lang(train_raw, ["<pad>", "<eos>"])
+
+    test_dataset = PennTreeBank(test_raw, lang)
+
+    test_loader = DataLoader(test_dataset, batch_size=config["batch_size_test"], 
+                             collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"]))
+
+    # Get vocab length
+    vocab_len = len(lang.word2id)
+
+    model = LM_LSTM(config["emb_size"], config["hid_size"], vocab_len, pad_index=lang.word2id["<pad>"]).to(DEVICE)
+    model.load_state_dict(torch.load(path_model_saved, map_location=DEVICE))
+
+    criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
+
+    # TESTING
+    final_ppl, _ = eval_loop(test_loader, criterion_eval, model)
+    print('Test ppl: ', final_ppl)
---
+++ exam/247181_simone_roman/LM/part_2/README.md
@@ -0,0 +1,16 @@
+# Language Model Project
+
+
+## Scripts and directory
+
+### 1. `main_manual_avsgd.py`
+
+This script implements the language model with manual Average Stochastic Gradient Descent (AvSGD). The manual version calculates the moving average of the model weights during training and mantain SGD optimizer.
+
+### 2. `main_automatic_avsgd.py`
+
+This script implements the language model using the automatic AvSGD available in PyTorch. The automatic version leverages PyTorch's built-in functionalities to switch from standard SGD to AvSGD during training.
+
+### 3. `extra`
+
+The folder `extra` contains details on how each model was trained, along with a graph of the loss. Additionally, there is a script `test_model.py` provided to test the various models.
